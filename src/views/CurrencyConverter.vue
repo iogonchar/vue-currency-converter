924ffb99af6e971d7269ce0aaa551b48f6667601
Ilya Gonchar <i.o.gonchar@yandex.ru>
2021-10-07 14:06:25 +0300

    project base structure

---
+++ src/views/CurrencyConverter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    test currency converter
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CurrencyConverter'
+  }
+</script>
+
+<style>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, 'src/main.js')
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src/index.html'),
+      inject: 'body'
+    }),
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin()
+  ]
+}
